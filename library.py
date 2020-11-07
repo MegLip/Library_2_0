1bca1b77c8255e83c9912c5cca6ab9baea12f012
Magdalena Lipińska <szymczyk.meg@gmail.com>
2020-11-07 03:46:29 +0100

    Add library file

---
+++ library.py
@@ -0,0 +1,15 @@
+from app import app, db
+from app.models import Book, Author, Borrowed
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+       "db": db,
+       "Book": Book,
+       "Author": Author,
+       "Borrowed": Borrowed}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
